111e60e54883cb9955f8307adec4ad0c90d22e3b
Sk1ye - Bits <85787056+sk1yebits@users.noreply.github.com>
2022-03-21 15:22:58 +0530

    Added Update & Delete Commands

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ class Library {
         void panel();
         void addBook();
         void viewBook();
+        void updateBook();
+		void del_book();
 };
 
 void Library::panel() {
@@ -17,8 +19,8 @@ void Library::panel() {
     cout << "\n\n\t\t\t\tAdmin Panel";
     cout << "\n\n 1. Add New Book";
     cout << "\n 2. View Books";
-    cout << "\n 3. Update Book (Coming Soon)";
-    cout << "\n 4. Delete Book (Coming Soon)";
+    cout << "\n 3. Update Book";
+    cout << "\n 4. Delete Book";
     cout << "\n 5. Exit.";
 };
 
@@ -63,6 +65,87 @@ void Library::viewBook() {
     }
 }
 
+void Library::updateBook() {
+	system("cls");
+	fstream file, file1;
+	int no_copy, no_co, count = 0;
+	string b_name, b_na, a_name, a_na, b_idd, b_id;
+	cout << "\n\n\t\t\t\tUpdate Book Record";
+	file1.open("D://book1.txt", ios::app | ios::out);
+	file.open("D://book.txt", ios::in);
+	if (!file)
+		cout << "\n\n File Openning Error...";
+	else
+	{
+		cout << "\n\n Book ID : ";
+		cin >> b_id;
+		file >> b_idd >> b_name >> a_name >> no_copy;
+		while (!file.eof())
+		{
+			if (b_id == b_idd)
+			{
+				system("cls");
+				cout << "\n\n\t\t\t\tUpdate Book Record";
+				cout << "\n\n New Book Name : ";
+				cin >> b_na;
+				cout << "\n\n\t\t\tAuthor Name : ";
+				cin >> a_na;
+				cout << "\n\n No. of Copies : ";
+				cin >> no_co;
+				file1 << " " << b_id << " " << b_na << " " << a_na << " " << no_co << "\n";
+				count++;
+			}
+			else
+				file1 << " " << b_idd << " " << b_name << " " << a_name << " " << no_copy << "\n";
+			file >> b_idd >> b_name >> a_name >> no_copy;
+		}
+		if (count == 0)
+			cout << "\n\n Book ID Not Found...";
+	}
+	file.close();
+	file1.close();
+	remove("D://book.txt");
+	rename("D://book1.txt", "D://book.txt");
+}
+
+void Library::del_book()
+{
+	system("cls");
+	fstream file, file1;
+	int no_copy, count = 0;
+	string b_id, b_idd, b_name, a_name;
+	cout << "\n\n\t\t\t\tDelete Book Record";
+	file1.open("D://book1.txt", ios::app | ios::out);
+	file.open("D://book.txt", ios::in);
+	if (!file)
+		cout << "\n\n File Openning Error...";
+	else
+	{
+		cout << "\n\n Book ID : ";
+		cin >> b_id;
+		file >> b_idd >> b_name >> a_name >> no_copy;
+		while (!file.eof())
+		{
+			if (b_id == b_idd)
+			{
+				system("cls");
+				cout << "\n\n\t\t\t\tDelete Book Record";
+				cout << "\n\n One Book is Deleted Successfully...";
+				count++;
+			}
+			else
+				file1 << " " << b_idd << " " << b_name << " " << a_name << " " << no_copy << "\n";
+			file >> b_idd >> b_name >> a_name >> no_copy;
+		}
+		if (count == 0)
+			cout << "\n\n Book ID Not Found...";
+	}
+	file.close();
+	file1.close();
+	remove("D://book.txt");
+	rename("D://book1.txt", "D://book.txt");
+}
+
 void main()
 {
     int choise;
@@ -78,11 +161,11 @@ void main()
             case 2:
                 lib.viewBook();
                 break;
-            case 3:
-                cout << "\nUpdate Book..";
+			case 3:
+				lib.updateBook();
                 break;
             case 4:
-                cout << "\nDelete book..";
+				lib.del_book();
                 break;
             case 5:
                 exit(0);
